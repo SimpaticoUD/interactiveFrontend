26cb548a6b1e58818f77c9b8fecf6b82a320570b
Enrique Sanz Yagüe <enriquesanz@gmail.com>
2016-09-28 16:29:27 +0200

    SimplifyButton

--- xunta/simpatico.js
+++ xunta/simpatico.js
@@ -4,18 +4,40 @@ function simplify(name)
 
   //var changedText = "Texto Simplificado para "+name;
 
-  jQuery.getJSON('http://baconipsum.com/api/?callback=?',
-    { 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'1' },
-    function(baconGoodness)
-  {
-    if (baconGoodness && baconGoodness.length > 0)
+  simplifyValue = document.getElementById('simplifySwitch').value;
+
+  console.log(simplifyValue);
+
+  if (simplifyValue == "simplifyOff") {
+    console.log("No simplify");
+  }else {
+    console.log("simplifying");
+    jQuery.getJSON('http://baconipsum.com/api/?callback=?',
+      { 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'1' },
+      function(baconGoodness)
     {
-      console.log(baconGoodness);
-      //jQuery('#'+name+'').html(baconGoodness);
-      document.getElementById(name).innerHTML = "<p>" + baconGoodness + "</p>";
-    }
-  });
+      if (baconGoodness && baconGoodness.length > 0)
+      {
+        console.log(baconGoodness);
+        //jQuery('#'+name+'').html(baconGoodness);
+        document.getElementById(name).innerHTML = "<p>" + baconGoodness + "</p>";
+      }
+    });
+
+  }
 
 
-  //$('#'+name+'').html(changedText);
+
+}
+
+function switchSimplify()
+{
+  currentvalue = document.getElementById('simplifySwitch').value;
+  if(currentvalue == "simplifyOff"){
+    console.log("OFF->ON");
+    document.getElementById("simplifySwitch").value="simplifyOn";
+  }else{
+    console.log("ON->OFF");
+    document.getElementById("simplifySwitch").value="simplifyOff";
+  }
 }
