7f3c0084a638b836defe23241b894a27a6177be7
Enrique Sanz <enrique.sanz@deusto.es>
2016-12-19 15:15:47 +0100

    Fadeout and fadein

--- simpatico.js
+++ simpatico.js
@@ -1,4 +1,4 @@
-var proxyURL = 'http://192.168.33.10';
+var proxyURL = 'http://asgard.deusto.es:52080/';
 
 var annotatedText = [];
 var paragraphs =[];
@@ -12,85 +12,49 @@ function pageLoaded() {
     //document.getElementById("simpatico_top").innerHTML = '<div id="simpatico_bar" style="background-color:yellow"> <img src="logo.png" height="50" width="50" alt="Simpatico" /><i class="fa fa-hand-scissors-o fa-3x"><input type="button" value="simplifyOff" id="simplifySwitch" onclick="switchSimplify();"></i><i class="fa fa-book fa-3x"><input type="button" value="defineOff" id="defineSwitch" onclick="switchDefine();"></i><i class="fa fa-pencil fa-3x"><input type="button" value="annotateOff" id="annotateSwitch" onclick="switchAnnotate();"></i><i class="fa fa-question fa-3x"><input type="button" value="citizenPediaOff" id="citizenPediaSwitch" onclick="switchCitizenPedia();"></i><i class="fa fa-user fa-3x"><input type="button" value="logIn" id="loginButton" onclick="handleAuthClick();"></i><button style="display:none" id="signout-button" onclick="handleSignoutClick()">Sign Out</button></div>';
     //document.getElementById("simpatico_top").innerHTML += '<style>.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 120px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 1s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:active .tooltiptext {visibility: visible;opacity: 1;}</style>';
 
-    simpaticoBarHtml = '<div id="simpatico_bar" style="background-color:yellow"> <img src="logo.png" height="50" width="50" alt="Simpatico" />';
+    simpaticoBarHtml = '<div id="simpatico_bar" style="background-color:#d3d3d6; position: fixed; top:0; width: 100%; z-index: 999;"> <img src="logo.png" height="50" width="50" alt="Simpatico" />';
     //'<i class="fa fa-hand-scissors-o fa-3x"><input type="button" value="simplifyOff" id="simplifySwitch" onclick="switchSimplify();"></i><i class="fa fa-book fa-3x"><input type="button" value="defineOff" id="defineSwitch" onclick="switchDefine();"></i><i class="fa fa-pencil fa-3x"><input type="button" value="annotateOff" id="annotateSwitch" onclick="switchAnnotate();"></i><i class="fa fa-question fa-3x"><input type="button" value="citizenPediaOff" id="citizenPediaSwitch" onclick="switchCitizenPedia();"></i><i class="fa fa-user fa-3x"><input type="button" value="logIn" id="loginButton" onclick="handleAuthClick();"></i><button style="display:none" id="signout-button" onclick="handleSignoutClick()">Sign Out</button></div>';
 
     for (var i = 0; i < buttons.length; i++) {
       console.log(buttons[i]);
-      simpaticoBarHtml += '<input style="bgcolor:white" type="button" value="'+ buttons[i]+'Off" id="'+buttons[i]+'Switch" onclick="switchFunction(\''+buttons[i]+'\');">'
+      //simpaticoBarHtml += '<input style="bgcolor:white;" src="img/simplify.png" type="button" value="'+ buttons[i]+'Off" id="'+buttons[i]+'Switch" onclick="switchFunction(\''+buttons[i]+'\');">'
+      simpaticoBarHtml += '<button type="submit" id="'+buttons[i]+'Switch" value="'+ buttons[i]+'Off" style="border: 0; background: transparent" onclick="switchFunction(\''+buttons[i]+'\');"><img src="img/'+ buttons[i]+'.png" width="50" height="50" alt="submit" /></button>';
     }
 
     simpaticoBarHtml += '</div>';
 
     document.getElementById("simpatico_top").innerHTML = simpaticoBarHtml;
     document.getElementById("simpatico_top").innerHTML += '<style>.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 120px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 1s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:active .tooltiptext {visibility: visible;opacity: 1;}</style>';
+    document.getElementById("loginSwitch").style.float = "right";
 
 }
 
 
 function switchFunction(functionName)
 {
+
   for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i] == functionName) {
-      document.getElementById(buttons[i]+'Switch').style.backgroundColor = '#FFFF00';
-      //document.getElementById(buttons[i]+'Switch').value = "pressed";
-    }else{
-      document.getElementById(buttons[i]+'Switch').style.backgroundColor = 'white';
-      //document.getElementById(buttons[i]+'Switch').value = buttons[i]+"Switch";
+    document.getElementById(buttons[i]+"Switch").style.borderLeft = "none";
+    if (buttons[i] === functionName) {
+        document.getElementById(functionName+"Switch").style.borderLeft = "thick solid #0000FF";
     }
+
   }
-  console.log("Calling switch"+functionName);
+
+  console.log("Calling "+"switch"+functionName);
   window["switch"+functionName]();
-  // console.log("Inside switch "+functionName);
-  // document.getElementById(functionName+'Switch').style.backgroundColor = '#FFFF00';
-  // document.getElementById(functionName+'Switch').value = "pressed";
 }
 
 
 ///////// OLD
 
 
-
-function checkButtons(name)
-{
-  simplifyValue = document.getElementById('simplifySwitch').value;
-
-  if (simplifyValue == "simplifyOn") {
-    simplify(name);
-  }
-
-  if(annotateValue == "annotateOn"){
-    annotate(name);
-  }
-
-  if (citizenPediaValue == 'citizenPediaOn') {
-    citizenpedia(name);
-  }
-}
-
-
 function simplify(name)
 {
-  simplifyValue = document.getElementById('simplifySwitch').value;
-
-  if (simplifyValue == "simplifyOff") {
-    console.log("No simplify");
-  }else {
-    console.log("simplifying");
-    // jQuery.getJSON('http://baconipsum.com/api/?callback=?',
-    //   { 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'1' },
-    //   function(baconGoodness)
-    // {
-    //   if (baconGoodness && baconGoodness.length > 0)
-    //   {
-    //     console.log(baconGoodness);
-    //     document.getElementById(name).innerHTML = "<p>" + baconGoodness + "</p>";
-    //   }
-    // });
 
     switch (name) {
       case "sp0":
-        var textReplace = '<li style="font-size: 1em; list-style: circle">a) Copia compulsada do documento nacional de identidade <span class="simp-text-term">(DNI)</span> ou ben do pasaporte ou do número de identificación de estranxeiro <span class="simp-text-term">(NIE)</span>.	</li>';
+        var textReplace = '<li style="font-size: 1em; list-style: circle;">a) Copia compulsada do documento nacional de identidade <span class="simp-text-term">(DNI)</span> ou ben do pasaporte ou do número de identificación de estranxeiro <span class="simp-text-term">(NIE)</span>.	</li>';
         break;
 
       case "sp1":
@@ -113,28 +77,67 @@ function simplify(name)
 
     }
 
-    document.getElementById(name).innerHTML = textReplace;
+    // var clonedDiv = document.getElementById(name).cloneNode(true);
+    // clonedDiv.id=name+"_simplified";
+    // clonedDiv.setAttribute("onclick", "closeSimp()");
+    // clonedDiv.innerHTML = textReplace;
+    // clonedDiv.style.position='absolute';
 
-  }
 
-  termsGetDefinition();
+    //document.getElementById(name).style.visibility='hidden';
+    fade(document.getElementById(name), textReplace, unfade);
+
+    //document.getElementById(name).parentNode.appendChild(clonedDiv);
+    //document.getElementById(name).parentNode.replaceChild(clonedDiv, document.getElementById(name));
+
+
+    //termsGetDefinition();
 
 }
 
-function annotate(name)
+function fade(element, text, callback) {
+  console.log("fade");
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+            callback(element,text);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+
+
+
+}
+
+function unfade(element,text) {
+  console.log("unfade");
+    element.innerHTML = text;
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function closeSimp()
 {
-
-  if (annotatedText[name] == undefined) { annotatedText[name] = "Add note"};
-
-  document.getElementById(name).innerHTML = document.getElementById(name).innerHTML +
-  '<span id="annotateGroup'+name+'"><textarea id="annotate" rows="4" cols="50">'+annotatedText[name]+'</textarea><input type="button" value="Send" id="sendAnnotate" onclick="sendAnnotate(\''+name+'\');"></span>';
-  document.getElementById("annotateSwitch").value="annotateOff";
+  console.log("cerrando");
 }
 
 function citizenpedia(name)
 {
   window.location.href = 'http://asgard.deusto.es:52180/questions/create';
 }
+
 function sendAnnotate(name)
 {
   annotatedText[name] = document.getElementById("annotate").value;
@@ -162,7 +165,8 @@ function switchsimplify()
         paragraphs[i].setAttribute("id", "sp"+i);
         var paragraph = document.getElementById(paragraphs[i].id);
         var paragraphName = paragraphs[i].id;
-        paragraphs[i].setAttribute("onclick", "checkButtons('"+paragraphName+"');");
+        paragraphs[i].style.position='relative';
+        paragraphs[i].setAttribute("onclick", "simplify('"+paragraphName+"');");
         paragraphs[i].style.borderLeft = "thick solid #0000FF";
         //paragraph.onclick = function(paragraphName) { checkButtons(paragraphName); };
       }
@@ -177,8 +181,19 @@ function switchsimplify()
   termsGetDefinition();
 }
 
+function switchforms()
+{
+  console.log("switchforms");
+}
+
+function switchcitizenpedia()
+{
+  console.log("switchcitizenpedia");
+}
+
 function termsGetDefinition()
 {
+  console.log("inside termsGetDefinition");
   terms = document.getElementsByClassName("simp-text-term");
 
   for (var t = 0, len = terms.length; t < len; t++) {
@@ -189,63 +204,6 @@ function termsGetDefinition()
     termToChange = document.getElementById(terms[t].id);
     changeTooltip(termToChange);
   }
-}
-
-function switchAnnotate()
-{
-  simplifyValue = document.getElementById('simplifySwitch').value;
-  annotateValue = document.getElementById('annotateSwitch').value;
-  citizenPediaValue = document.getElementById('citizenPediaSwitch').value;
-  defineValue = document.getElementById('defineSwitch').value;
-
-  if (simplifyValue == "simplifyOn") {
-    document.getElementById("simplifySwitch").value="simplifyOff";
-  }
-
-  if (citizenPediaValue == "citizenPediaOn") {
-    document.getElementById("citizenPediaSwitch").value="citizenPediaOff";
-  }
-
-  if (defineValue == "defineOn") {
-    document.getElementById("defineSwitch").value="defineOff";
-  }
-
-  if(annotateValue == "annotateOff"){
-    document.getElementById("annotateSwitch").value="annotateOn";
-  }else{
-    document.getElementById("annotateSwitch").value="annotateOff";
-  }
-
-
-}
-
-function switchCitizenPedia()
-{
-  simplifyValue = document.getElementById('simplifySwitch').value;
-  annotateValue = document.getElementById('annotateSwitch').value;
-  citizenPediaValue = document.getElementById('citizenPediaSwitch').value;
-  defineValue = document.getElementById('defineSwitch').value;
-
-
-  if (simplifyValue == "simplifyOn") {
-    document.getElementById("simplifySwitch").value="simplifyOff";
-  }
-
-  if (annotateValue == "annotateOn") {
-    document.getElementById("annotateSwitch").value="annotateOff";
-  }
-
-  if (defineValue == "defineOn") {
-    document.getElementById("defineSwitch").value="defineOff";
-  }
-
-  if(citizenPediaValue == "citizenPediaOff"){
-    document.getElementById("citizenPediaSwitch").value="citizenPediaOn";
-  }else{
-    document.getElementById("citizenPediaSwitch").value="citizenPediaOff";
-  }
-
-
 }
 
 
