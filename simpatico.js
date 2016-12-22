ac970ae0d322444c20816ac41e6a79df0a1e88f7
Enrique Sanz <enrique.sanz@deusto.es>
2016-12-22 14:18:27 +0100

    Citizenpedia button

--- simpatico.js
+++ simpatico.js
@@ -2,8 +2,10 @@ var proxyURL = 'https://simpatico.morelab.deusto.es/';
 
 var annotatedText = [];
 var paragraphs =[];
+var questionsHtml;
 
 var buttons = ["simplify", "forms", "citizenpedia","login"];
+var functionsColors = {"simplify":"#0000FF", "forms":"#de453e", "citizenpedia":"#379e4c","login":"#0000FF"};
 
 var savedParagraph;
 
@@ -15,7 +17,6 @@ function pageLoaded() {
     //document.getElementById("simpatico_top").innerHTML += '<style>.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 120px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 1s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:active .tooltiptext {visibility: visible;opacity: 1;}</style>';
 
     simpaticoBarHtml = '<div id="simpatico_bar" style="background-color:#d3d3d6; position: fixed; top:0; width: 100%; z-index: 999;"> <img src="logo.png" height="50" width="50" alt="Simpatico" />';
-    //'<i class="fa fa-hand-scissors-o fa-3x"><input type="button" value="simplifyOff" id="simplifySwitch" onclick="switchSimplify();"></i><i class="fa fa-book fa-3x"><input type="button" value="defineOff" id="defineSwitch" onclick="switchDefine();"></i><i class="fa fa-pencil fa-3x"><input type="button" value="annotateOff" id="annotateSwitch" onclick="switchAnnotate();"></i><i class="fa fa-question fa-3x"><input type="button" value="citizenPediaOff" id="citizenPediaSwitch" onclick="switchCitizenPedia();"></i><i class="fa fa-user fa-3x"><input type="button" value="logIn" id="loginButton" onclick="handleAuthClick();"></i><button style="display:none" id="signout-button" onclick="handleSignoutClick()">Sign Out</button></div>';
 
     for (var i = 0; i < buttons.length; i++) {
       console.log(buttons[i]);
@@ -23,7 +24,7 @@ function pageLoaded() {
       simpaticoBarHtml += '<button type="submit" id="'+buttons[i]+'Switch" value="'+ buttons[i]+'Off" style="visibility: hidden; border: 0; background: transparent" onclick="switchFunction(\''+buttons[i]+'\');"><img id="'+ buttons[i]+'img" src="img/'+ buttons[i]+'.png" width="50" height="50" alt="submit" /></button>';
     }
 
-    simpaticoBarHtml += '<div id="userdata"></div>';
+    simpaticoBarHtml += '<span id="userdata"></span>';
 
     simpaticoBarHtml += '</div>';
 
@@ -32,6 +33,7 @@ function pageLoaded() {
     document.getElementById("loginSwitch").style.float = "right";
     document.getElementById("loginSwitch").style.visibility = "visible";
 
+    document.getElementById("userdata").style.float = "right";
 }
 
 
@@ -61,7 +63,7 @@ function switchFunction(functionName)
     for (var i = 0; i < buttons.length; i++) {
       document.getElementById(buttons[i]+"Switch").style.borderLeft = "none";
       if (buttons[i] === functionName) {
-          document.getElementById(functionName+"Switch").style.borderLeft = "thick solid #0000FF";
+          document.getElementById(functionName+"Switch").style.borderLeft = "thick solid " + functionsColors[functionName];
       }
 
     }
@@ -76,7 +78,7 @@ function simplify(name)
 {
     savedParagraph = document.getElementById(name).innerHTML;
 
-    document.getElementById(name).style.borderBottom = "thick solid #0000FF";
+    document.getElementById(name).style.borderBottom = "thick solid " + functionsColors["simplify"];
 
     switch (name) {
       case "sp0":
@@ -103,74 +105,105 @@ function simplify(name)
 
     }
 
-    // var clonedDiv = document.getElementById(name).cloneNode(true);
-    // clonedDiv.id=name+"_simplified";
-    // clonedDiv.setAttribute("onclick", "closeSimp()");
-    // clonedDiv.innerHTML = textReplace;
-    // clonedDiv.style.position='absolute';
-
+    var clonedDiv = document.getElementById(name).cloneNode(true);
+    //var clonedDiv = document.createElement('div');
+    clonedDiv.id=name+"_simplified";
+    clonedDiv.setAttribute("onclick", "closeSimp()");
+    clonedDiv.innerHTML = textReplace;
+    clonedDiv.style.position='relative';
 
     //document.getElementById(name).style.visibility='hidden';
 
-    document.getElementById(name).setAttribute("onclick", "closeSimp(\'"+document.getElementById(name).id+"\')");
+    //document.getElementById(name).parentNode.replaceChild(clonedDiv, document.getElementById(name));
 
-    fade(document.getElementById(name), textReplace, unfade);
+    // document.getElementById(name).setAttribute("onclick", "closeSimp(\'"+document.getElementById(name).id+"\')");
+    //
+    // fade(document.getElementById(name), textReplace, unfade);
 
     //document.getElementById(name).parentNode.appendChild(clonedDiv);
-    //document.getElementById(name).parentNode.replaceChild(clonedDiv, document.getElementById(name));
+    //move(document.getElementById(name));
+
+    fadeOut(document.getElementById(name), textReplace);
+
 
 
     termsGetDefinition();
 
 }
 
-function fade(element, text, callback) {
-  console.log("fade");
+function fadeOut(element, textReplace) {
+  console.log("fadeOut");
+    var left = 10;
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
             element.style.display = 'none';
-            callback(element,text);
+            var opa = 0.1;  // initial opacity
+            element.style.display = 'block';
+            var timer2 = setInterval(function () {
+                if (opa >= 1){
+                    clearInterval(timer2);
+                }
+                element.innerHTML = textReplace;
+                element.setAttribute("onclick", "closeSimp('"+element.id+"')");
+                element.style.opacity = opa;
+                element.style.filter = 'alpha(opacity=' + opa * 100 + ")";
+                element.style.left = left + 'px' // show frame
+                opa += opa * 0.1;
+                left-=10;
+            }, 20);
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.left = left + 'px' // show frame
+
         op -= op * 0.1;
-    }, 50);
-
-
-
+        left+=10;
+    }, 20);
 }
 
-function unfade(element,text) {
-
-    element.innerHTML = text;
-
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
+function fadeIn(element, savedParagraph) {
+  console.log("fadeOut");
+    var left = 10;
+    var op = 1;  // initial opacity
     var timer = setInterval(function () {
-        if (op >= 1){
+        if (op <= 0.1){
             clearInterval(timer);
+            element.style.display = 'none';
+
+            var opa = 0.1;  // initial opacity
+            element.style.display = 'block';
+            var timer2 = setInterval(function () {
+                if (opa >= 1){
+                    clearInterval(timer2);
+                }
+                element.innerHTML = savedParagraph;
+                element.setAttribute("onclick", "simplify('"+element.id+"')");
+                element.style.borderBottom = "none";
+                element.style.opacity = opa;
+                element.style.filter = 'alpha(opacity=' + opa * 100 + ")";
+                element.style.left = left + 'px' // show frame
+                opa += opa * 0.1;
+                left-=10;
+            }, 20);
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
+        element.style.left = left + 'px' // show frame
+
+        op -= op * 0.1;
+        left+=10;
+    }, 20);
 }
+
 
 function closeSimp(name)
 {
-  document.getElementById(name).setAttribute("onclick", "simplify('"+name+"');");
-  document.getElementById(name).style.borderBottom = "none";
-  fade(document.getElementById(name), savedParagraph, unfade);
+  fadeIn(document.getElementById(name), savedParagraph);
   termsGetDefinition();
 
 
-}
-
-function citizenpedia(name)
-{
-  window.location.href = 'http://asgard.deusto.es:52180/questions/create';
 }
 
 // Buttons
@@ -180,7 +213,9 @@ function switchsimplify()
   simplifyValue = document.getElementById('simplifySwitch').value;
 
   // Search for paragraphs
-  paragraphs = document.getElementsByClassName("simp-text-paragraph");
+  if (paragraphs.length == 0) {
+    paragraphs = document.getElementsByClassName("simp-text-paragraph");
+  }
 
   if(simplifyValue == "simplifyOff"){
     document.getElementById("simplifySwitch").value="simplifyOn";
@@ -193,7 +228,7 @@ function switchsimplify()
         var paragraphName = paragraphs[i].id;
         paragraphs[i].style.position='relative';
         paragraphs[i].setAttribute("onclick", "simplify('"+paragraphName+"');");
-        paragraphs[i].style.borderLeft = "thick solid #0000FF";
+        paragraphs[i].style.borderLeft = "thick solid " + functionsColors["simplify"];
         //paragraph.onclick = function(paragraphName) { checkButtons(paragraphName); };
       }
 
@@ -212,10 +247,66 @@ function switchforms()
   console.log("switchforms");
 }
 
+
+/////////////////////////////////// CITIZENPEDIA
+
 function switchcitizenpedia()
 {
   console.log("switchcitizenpedia");
+  if (document.getElementById('simplifySwitch').value == "simplifyOn") {
+      switchFunction("simplify");
+  }
+
+  if (paragraphs.length == 0) {
+    paragraphs = document.getElementsByClassName("simp-text-paragraph");
+  }
+
+  for (var i = 0, len = paragraphs.length; i < len; i++) {
+    console.log("tema");
+    paragraphs[i].setAttribute("id", "sp"+i);
+    var paragraph = document.getElementById(paragraphs[i].id);
+    var paragraphName = paragraphs[i].id;
+    paragraphs[i].style.position='relative';
+    paragraphs[i].setAttribute("onclick", "citizenpedia('"+paragraphName+"');");
+    paragraphs[i].style.borderLeft = "thick solid " + functionsColors["citizenpedia"];
+    //paragraph.onclick = function(paragraphName) { checkButtons(paragraphName); };
+  }
+
+
+} //switchcitizenpedia()
+
+function citizenpedia(name)
+{
+  console.log("citizenpedia "+name);
+
+  // Create questions div
+  var questionsDiv = document.createElement('div');
+  questionsDiv.id=name + "_questions";
+  questionsDiv.style.borderLeft = "thick solid " + functionsColors["citizenpedia"];
+  questionsDiv.style.borderTop = "thick solid " + functionsColors["citizenpedia"];
+  questionsDiv.style.backgroundColor = "#a9a7a7";
+
+  questionsHtml = "QUESTIONS:<ul>";
+
+  jQuery.getJSON(proxyURL+'/interactiveFrontend/questions.json',
+    function(jsonResponse)
+    {
+      for (var q = 0; q < Object.keys(jsonResponse.questions).length; q++) {
+        //console.log(Object.values(jsonResponse.questions)[q]);
+        //console.log("<li><a href=\""+ Object.values(jsonResponse.questions)[q].url + "\">" + Object.values(jsonResponse.questions)[q].title + "</a></li>");
+        questionsHtml += "<li><a href=\""+ Object.values(jsonResponse.questions)[q].url + "\">" + Object.values(jsonResponse.questions)[q].title + "</a></li>";
+
+      }
+      questionsHtml += "<li><a href=\"http://www.google.es\">Add New Question</a></li>";
+      questionsHtml += "</ul>";
+      questionsDiv.innerHTML = questionsHtml;
+      document.getElementById(name).appendChild(questionsDiv);
+    });
+
+  //window.location.href = 'http://asgard.deusto.es:52180/questions/create';
 }
+
+/////////////////////////////////// CITIZENPEDIA
 
 function termsGetDefinition()
 {
@@ -259,4 +350,59 @@ function cancelClick(e)
   if (!e) var e = window.event;
   e.cancelBubble = true;
   if (e.stopPropagation) e.stopPropagation();
+}
+
+///////////////////////////////////
+// GOOGLE OAUTH
+function handleClientLoad() {
+  // Load the API client and auth2 library
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  gapi.client.init({
+      apiKey: 'AIzaSyABSlDxVQ1yv_4kGEMcazYMu-X3NJg8yZA',
+      discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+      clientId: '167570783497-frbg9dr5da757u3a35rln8elrak51euj.apps.googleusercontent.com',
+      scope: 'profile'
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  });
+}
+
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    makeApiCall();
+  }
+}
+
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+  document.getElementById('loginimg').src = "img/login.png";
+  document.getElementById('loginSwitch').setAttribute("onclick", "switchFunction('login');");
+  document.getElementById('userdata').innerHTML = "";
+  hideButtons();
+
+}
+
+function makeApiCall() {
+  gapi.client.people.people.get({
+    resourceName: 'people/me'
+  }).then(function(resp) {
+    document.getElementById('userdata').innerHTML = 'Hello, ' + resp.result.names[0].givenName;
+    document.getElementById('userdata').style = "display:block";
+    document.getElementById('loginimg').src = "img/ic_on.png";
+    document.getElementById('loginSwitch').setAttribute("onclick", "handleSignoutClick()");
+    showButtons();
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
 }
